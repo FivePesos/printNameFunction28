0e79f062f249ac36ed171787df6b468ad6fad933
SingCow <75522316+FivePesos@users.noreply.github.com>
2023-11-07 20:07:13 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printName();
+int main()
+{
+    printName();
+    return 0;
+}
+
+void printName(){
+    printf("*****************************\n\n");
+    printf("*                           *\n\n");
+    printf("*    John Paul B. Singco    *\n\n");
+    printf("*                           *\n\n");
+    printf("*****************************\n\n");
+}
